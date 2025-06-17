d390278585f6abc8fcef43e43c630526ce809a71
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2025-06-16 22:08:33 -0400

    Update Reminder.cs

--- Sync/Models/Reminder.cs
+++ Sync/Models/Reminder.cs
@@ -1,6 +1,25 @@
-﻿namespace Sync.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Sync.Models
 {
     public class Reminder
     {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+
     }
 }
