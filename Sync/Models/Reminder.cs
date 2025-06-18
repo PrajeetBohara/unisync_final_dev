6cb258c61a6de40759cad4177421a9ea5b17348c
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2025-06-17 23:43:30 -0400

    Update Reminder.cs

--- Sync/Models/Reminder.cs
+++ Sync/Models/Reminder.cs
@@ -1,8 +1,40 @@
-﻿using System;
+﻿//using System;
+//using System.ComponentModel.DataAnnotations;
+
+
+//namespace Sync.Models
+//{
+//    public class Reminder
+//    {
+//        public int Id { get; set; }
+
+//        [Required, StringLength(100)]
+//        public string Title { get; set; } = string.Empty;
+
+//        public string? Description { get; set; }
+
+//        [Required]
+//        public DateTime DueDate { get; set; }
+
+//        [Required]
+//        public int CategoryId { get; set; }
+//        public Category? Category { get; set; }
+
+
+//    }
+//}
+
+
+
+using Sync.Models;
+using System.ComponentModel.DataAnnotations;
+using Unisync.Models;
+
+**Models / Reminder.cs * *
+using System;
 using System.ComponentModel.DataAnnotations;
 
-
-namespace Sync.Models
+namespace Unisync.Models
 {
     public class Reminder
     {
@@ -16,10 +48,9 @@ namespace Sync.Models
         [Required]
         public DateTime DueDate { get; set; }
 
+        // Category relationship (foreign key and navigation property)
         [Required]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
-
-
     }
 }
