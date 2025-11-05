2f67ea21af1988b5a46fd4e76a7eb269c4ebefc1
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2025-11-04 23:31:42 -0600

    Update HomeController.cs
    copied the classes for future updates

--- Sync/Controllers/HomeController.cs
+++ Sync/Controllers/HomeController.cs
@@ -1,3 +1,37 @@
+// using System.Diagnostics;
+// using Microsoft.AspNetCore.Mvc;
+// using Sync.Models;
+
+// namespace Sync.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+//         private readonly ILogger<HomeController> _logger;
+
+//         public HomeController(ILogger<HomeController> logger)
+//         {
+//             _logger = logger;
+//         }
+
+//         public IActionResult Index()
+//         {
+//             return View();
+//         }
+
+//         public IActionResult Privacy()
+//         {
+//             return View();
+//         }
+
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         public IActionResult Error()
+//         {
+//             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//         }
+//     }
+// }
+
+
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Sync.Models;
